085f1ca86e0f0361b3cfb86314fcf97e4e0e74c7
Chang9601 <changsup96@gmail.com>
2023-02-17 15:59:54 +0900

    [ADD] 유저 정보 (게시물 목록 포함) 엔드포인트

--- app.js
+++ app.js
@@ -28,7 +28,58 @@ const posts = [
   },
 ];
 
-const getAggregatedData = (users, posts) => {
+/* 깊은 복사 */
+const cloneObject = (obj) => {
+  let clone = {};
+  for (let key in obj) {
+    if (typeof obj[key] === "object" && obj[key] != null) {
+      clone[key] = cloneObject(obj[key]);
+    } else {
+      clone[key] = obj[key];
+    }
+  }
+
+  return clone;
+};
+
+/* -1인 경우 예외 처리 필요 */
+const getIndex = (id, data) => {
+  const idx = data.findIndex((datum) => {
+    return datum.id === id;
+  });
+
+  return idx;
+};
+
+/* 사용자의 모든 게시물 */
+const getUserPosts = (idx, posts) => {
+  const user = cloneObject(users[idx]);
+
+  user.postings = [];
+
+  user.userID = user.id;
+  user.userName = user.name;
+
+  posts.forEach((post) => {
+    if (user.id === post.userId) {
+      user.postings.push({
+        postingId: post.id,
+        postingName: post.title,
+        postingContent: post.content,
+      });
+    }
+  });
+
+  delete user.email;
+  delete user.password;
+  delete user.id;
+  delete user.name;
+
+  return user;
+};
+
+/* 모든 게시물 */
+const getPosts = (users, posts) => {
   const aggregatedData = [];
 
   posts.forEach((post) => {
@@ -54,9 +105,25 @@ const server = http.createServer();
 const httpRequestListener = (request, response) => {
   if (request.method === "GET") {
     if (request.url === "/posts") {
-      const data = getAggregatedData(users, posts);
+      const data = getPosts(users, posts);
       response.writeHead(200, { "Content-Type": "application/json" });
       response.end(JSON.stringify({ data: data }));
+    } else if (request.url === "/users") {
+      let body = "";
+
+      request.on("data", (data) => {
+        body += data;
+      });
+
+      request.on("end", () => {
+        let id = JSON.parse(body);
+        id = parseInt(id.id);
+        const idx = getIndex(id, users);
+        const data = getUserPosts(idx, posts);
+
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ data: data }));
+      });
     }
   } else if (request.method === "POST") {
     if (request.url === "/user") {
@@ -110,16 +177,17 @@ const httpRequestListener = (request, response) => {
 
       request.on("end", () => {
         const update = JSON.parse(body);
-        const id = parseInt(update.id) - 1;
+        const id = parseInt(update.id);
+        const idx = getIndex(id, posts);
 
-        const post = posts[id];
+        const post = posts[idx];
         post.title = update.title;
         post.content = update.content;
 
-        const data = getAggregatedData(users, posts);
+        const data = getPosts(users, posts);
 
         response.writeHead(200, { "Content-Type": "application/json" });
-        response.end(JSON.stringify({ data: data[id] }));
+        response.end(JSON.stringify({ data: data[idx] })); // posts 배열과 같은 순서
       });
     }
   } else if (request.method === "DELETE") {
@@ -132,12 +200,13 @@ const httpRequestListener = (request, response) => {
 
       request.on("end", () => {
         let id = JSON.parse(body);
-        id = parseInt(id.id) - 1;
+        id = parseInt(id.id);
+        const idx = getIndex(id, posts);
 
-        posts.splice(id, 1);
+        posts.splice(idx, 1);
 
         response.writeHead(200, { "Content-Type": "application/json" });
-        response.end(JSON.stringify({ data: posts }));
+        response.end(JSON.stringify({ message: "postingDeleted" }));
       });
     }
   }
